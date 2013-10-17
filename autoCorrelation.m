265aa86b62bfbbaf9562defc0b651787897c8699
Emil Loevbak <emil.loevbak@gmail.com>
2013-10-17 16:14:48 +0200

    Werkt nu voor zover ik kan zien.

--- autoCorrelation.m
+++ autoCorrelation.m
@@ -7,8 +7,8 @@ function pitch = autoCorrelation( frame, sampleRate )
 %         -sampleRate: The sample rate at which the sound was recorded.
 %   Output:-pitch: The average pitch of the providid frame of sound.
 
-MIN_THRESHOLD = 0.3;%How much smaller than the maximum correlation value a point has to be before we assume a peak has been passed.
-MAX_THRESHOLD = 0.7;%How much larger that the previous maximum correlation value a point has to be before we assume that the correlation signas is rising again.
+MIN_THRESHOLD = 0.10;%How much smaller than the maximum correlation value a point has to be before we assume a peak has been passed.
+MAX_THRESHOLD = 0.90;%How much larger that the previous maximum correlation value a point has to be before we assume that the correlation signas is rising again.
 minReached = 0;%Boolean to determin whether this is the first minimum reached
 minPassed = 0;%Boolean to determin whether the second peak has been passed
 
@@ -17,7 +17,6 @@ maxCorrelationIndex = 0;
 prevMaxCorrelation = 0;
 
 for i = 2:size(frame, 2)%Same position guarentees a match so we start at 2.
-
     lengthOverlap = size(frame, 2) - i + 1;
     correlation = sum(frame(1:lengthOverlap) .* frame(i:size(frame, 2)))./lengthOverlap;
     if correlation > maxCorrelation
@@ -41,7 +40,8 @@ for i = 2:size(frame, 2)%Same position guarentees a match so we start at 2.
         if correlation > prevMaxCorrelation.*MAX_THRESHOLD
             minPassed = 1;
         
-    end
+        end%if
+    end%if
 end%for
 
 pitch = sampleRate./maxCorrelationIndex;%Freqency is the number of samples per second devided by the number of samples between peaks.
