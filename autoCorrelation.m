428e3c5e11e98b136ba05bd54e55e594760719c6
Emil Loevbak <emil.loevbak@gmail.com>
2013-10-17 15:44:41 +0200

    Gedebugd

--- autoCorrelation.m
+++ autoCorrelation.m
@@ -7,32 +7,40 @@ function pitch = autoCorrelation( frame, sampleRate )
 %         -sampleRate: The sample rate at which the sound was recorded.
 %   Output:-pitch: The average pitch of the providid frame of sound.
 
-THRESHOLD = 0.5;%How much smaller than the maximum correlation value a point has to be before we assume a peak has been passed.
+MIN_THRESHOLD = 0.3;%How much smaller than the maximum correlation value a point has to be before we assume a peak has been passed.
+MAX_THRESHOLD = 0.7;%How much larger that the previous maximum correlation value a point has to be before we assume that the correlation signas is rising again.
 minReached = 0;%Boolean to determin whether this is the first minimum reached
+minPassed = 0;%Boolean to determin whether the second peak has been passed
 
 maxCorrelation = 0;
 maxCorrelationIndex = 0;
+prevMaxCorrelation = 0;
 
-for i = 2:size(frame, 2)%Same position guarentees a match
-    
-    lengthOverlap = size(frame, 2) - i+1;
+for i = 2:size(frame, 2)%Same position guarentees a match so we start at 2.
+
+    lengthOverlap = size(frame, 2) - i + 1;
     correlation = sum(frame(1:lengthOverlap) .* frame(i:size(frame, 2)))./lengthOverlap;
     if correlation > maxCorrelation
         maxCorrelation = correlation;
         maxCorrelationIndex = i;
-    elseif correlation < maxCorrelation.*THRESHOLD%A maximum has definately been passed.
+    elseif correlation < maxCorrelation.*MIN_THRESHOLD%A maximum has definately been passed.
         %The following if statement makes sure that the first following
         %maximum is selected.
-        if minReached == 1%This is the second time that a maximum has been passed.
+        if minPassed == 1%This is the second time that a maximum has been passed.
             break
         else%This is the first time a maximum has been passed.
             minReached = 1;
             %Set maxCorrelation and its index to a lower value incase the
             %correlation at the next peak is less than a correlation value
             %for a point close to the first peak.
+            prevMaxCorrelation = maxCorrelation;
             maxCorrelation = correlation;
             maxCorrelationIndex = i;
         end
+    elseif prevMaxCorrelation ~=0%Sets the flag for passing the first minimum to true.
+        if correlation > prevMaxCorrelation.*MAX_THRESHOLD
+            minPassed = 1;
+        
     end
 end%for
 
